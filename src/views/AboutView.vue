82c13bcb1e8334ec1a12fc822bfbbe7e46634aa1
Sthanasis <sthanasis1994@gmail.com>
2022-02-17 10:48:48 +0200

    feature: Add home and about views

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+  <h1>This is the about page</h1>
+</template>
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Button from '../components/UI/Button/Button.vue';
+</script>
+<template>
+  <div
+    style="
+      height: 200px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    "
+  >
+    <div>
+      <Button btnType="danger">Test</Button>
+    </div>
+  </div>
+  <h1>This is the home page</h1>
+</template>
+<style></style>
